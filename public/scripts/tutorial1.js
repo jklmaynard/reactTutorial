830eea118ef776cc727a6a4a9847897369ed5a4f
James Maynard <jameskirkmaynard@gmail.com>
2015-04-13 11:26:47 -0700

    Add componentDidMount method to CommentBox.

--- public/scripts/tutorial1.js
+++ public/scripts/tutorial1.js
@@ -1,12 +1,23 @@
 var converter = new Showdown.converter();
-var data = [
-  {author: "Jane Brown", text: "This is one comment."},
-  {author: "John White", text: "This is *another* comment."}
-]
+
 var CommentBox = React.createClass({
   getInitialState: function() {
     return {data: []};
   },
+
+  compnentDidMount: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+
   render: function() {
     return (
       <div className="commentBox">
